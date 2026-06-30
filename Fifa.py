6b4ce9b18034e1ed05137ec51e0c466de6a57d64
ALEX HUNTER <alexhuntercct@gmail.com>
2026-06-30 17:44:23 +0600

    Update Fifa.py

--- Fifa.py
+++ Fifa.py
@@ -1,1 +1,11 @@
 
+import alex_enc.py)
+import inspect
+
+try:
+    print("Auto_run arguments:", inspect.signature(Tarmuxset.auto_run))
+except:
+    print("Signature not found. Trying main...")
+
+# যদি main কাজ করে তবে এটিই ব্যবহার করুন
+alex_enc.py).main()
